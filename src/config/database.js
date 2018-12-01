030d5a907784ed0f51d3ad2d2ee6e6eed4797ea3
tonypera <paulurbian@hotmail.com>
2018-12-01 17:26:05 -0500

    make it better

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
 	'url' : 'mongodb://login:login94@ds123434.mlab.com:23434/nodelogin'
-};
+	// 'url': 'mongodb://heroku_1kktm3dq:calisTONY94@ds123444.mlab.com:23444/heroku_1kktm3dq'
+};	
